05d3ca71cdb2b3e9dad53c57b1c55edf441540b7
Lara <larabonatto38@gmail.com>
2026-03-04 16:15:38 -0300

    terminei tudo

--- lp2_listas/src/Lista_matrizes/Ex_7.java
+++ lp2_listas/src/Lista_matrizes/Ex_7.java
@@ -1,7 +1,7 @@
 package Lista_matrizes;
 
 import java.util.Scanner;
-//ficou pra fazer na sala de aula
+
 //7) Escreva um programa que receba duas matrizes A(4,6) e B(6,4) e retorne uma matriz C, que
 //seja o produto matricial de M por N.
 public class Ex_7 {
@@ -9,26 +9,35 @@ public class Ex_7 {
     public static Scanner LER = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int[][] matA = new int[4][6];
-        int[][] matB = new int[6][4];
         int[][] matC = new int[4][4];
+        imprimir("\ndigite o tamanho da matriz A(i e j): ");
+        int tamanhoAI = lerInt();
+        int tamanhoAJ = lerInt();
+        int[][] matA= new int[tamanhoAI][tamanhoAJ];
 
-        imprimir("\ndigite os valores da matriz A 4X6: ");
-        matA = lerMatriz(matA);
+        imprimir("\ndigite o tamanho da matriz B(i e j): ");
+        int tamanhoBI = lerInt();
+        int tamanhoBJ = lerInt();
+        int[][] matB= new int[tamanhoBI][tamanhoBJ];
 
-        imprimir("\ndigite os valores da matriz B 6X4: ");
+        imprimir("\ndigite os valores da matriz A: ");
+        matA = lerMatriz(matA); 
+
+        imprimir("\ndigite os valores da matriz B: ");
         matB = lerMatriz(matB);
 
-        matC = multiplicarMatrizes(matA, matB);
+        matC = multiplicarMatrizes(matA, matB, tamanhoAI, tamanhoBJ);
 
         imprimirMatriz(matC);
     }
 
-    public static int[][] multiplicarMatrizes(int[][] A, int[][] B) {
-        int[][]C = new int[4][4];
+    public static int[][] multiplicarMatrizes(int[][] A, int[][] B, int tamanhoAI, int tamanhoBJ) {
+        int[][] C = new int[tamanhoAI][tamanhoBJ];
         for (int i = 0; i < C.length; i++) {
             for (int j = 0; j < C.length; j++) {
-                
+                for (int k = 0; k < C.length; k++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                } 
             }
         }
         return C;
